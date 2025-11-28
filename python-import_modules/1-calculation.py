2de7c54af83b18d0b42dac719728ab7ef1b5e4b0
root <Nada.Ghannam19@gmail.com>
2025-11-28 15:21:41 +0000

    Task 1: import add, sub, mul, div functions properly without * or __import__

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-import calculator_1 as cal
+from calculator_1 import add, sub, mul, div
+
 a = 10
 b = 5
-add = cal.add(a, b)
-sub = cal.sub(a, b)
-mul = cal.mul(a, b)
-div = cal.div(a, b)
-print("{} + {} = {}".format(a, b, add))
-print("{} - {} = {}".format(a, b, sub))
-print("{} * {} = {}".format(a, b, mul))
-print("{} / {} = {}".format(a, b, div))
+
+print(f"{a} + {b} = {add(a, b)}")
+print(f"{a} - {b} = {sub(a, b)}")
+print(f"{a} * {b} = {mul(a, b)}")
+print(f"{a} / {b} = {div(a, b)}")
