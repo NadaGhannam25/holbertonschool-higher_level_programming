e3e4461129df6d81e3aac89acbf39bf3cbe2f1d7
root <Nada.Ghannam19@gmail.com>
2026-01-16 18:34:40 +0000

    Filter states by user input (unsafe SQL query)

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
+"""
+This script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa where name matches the argument
+"""
+
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
+    """
+    Access to the database and get the states
+    from the database.
+    """
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
 
-    db = MySQLdb.connect(
-        host="localhost",
-        user=username,
-        passwd=password,
-        db=database,
-        port=3306
-    )
     cursor = db.cursor()
-
-    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC;".format(state_name)
-    cursor.execute(query)
-
+    cursor.execute("SELECT `id`, `name` \
+                    FROM `states` \
+                    WHERE `name` LIKE BINARY '{}' \
+                    ORDER BY states.id ASC".format(argv[4]))
     rows = cursor.fetchall()
+
     for row in rows:
         print(row)
-
-    cursor.close()
-    db.close()
-
