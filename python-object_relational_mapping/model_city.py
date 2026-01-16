f6c41557aa7ab80d7e34fcf4b730c4a07a10c6ac
root <Nada.Ghannam19@gmail.com>
2026-01-16 21:40:54 +0000

    Fix City-State ORM relationship and fetch cities by state

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Module that defines the City class."""
 
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from model_state import Base
 
 
 class City(Base):
@@ -14,3 +13,4 @@ class City(Base):
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
