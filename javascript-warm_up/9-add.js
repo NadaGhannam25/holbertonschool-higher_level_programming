8fb4c227e461eb714c6052e64a5d8c96c7ee7ddb
root <Nada.Ghannam19@gmail.com>
2026-01-29 01:15:46 +0000

    Add script with add(a, b) function to sum two integers

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,7 +1,10 @@
-#!/usr/bin/node
-function add(a, b) {
+#!/usr/bin/env node
+
+function add (a, b) {
   return a + b;
 }
+
 const a = parseInt(process.argv[2]);
 const b = parseInt(process.argv[3]);
+
 console.log(add(a, b));
