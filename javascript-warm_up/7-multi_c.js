81ffafb1ee14df6b8fb5ee084b7a85cd64979fb9
root <Nada.Ghannam19@gmail.com>
2026-01-29 00:56:48 +0000

    Add script to print 'C is fun' x times with validation

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 
 const x = parseInt(process.argv[2]);
-
 if (isNaN(x) || x <= 0) {
   console.log('Missing number of occurrences');
 } else {
@@ -9,4 +8,3 @@ if (isNaN(x) || x <= 0) {
     console.log('C is fun');
   }
 }
-
